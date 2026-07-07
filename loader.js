2f1731f2a84d43cb11b1c0ab6315093637d076f8
gameland125 <farzad03x@gmail.com>
2026-07-07 21:30:50 +0330

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,4 @@
+// loader.js
 function loadPayload(path) {
-    fetch(path)
-    .then(res => res.arrayBuffer())
-    .then(buf => {
-        console.log("Payload Loaded");
-        // اینجا inject واقعی انجام میشه
-    });
+   // fetch + inject
 }
