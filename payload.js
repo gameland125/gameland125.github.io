a84c8b0b2088b102c242532f4204d0bbf0329748
gameland125 <farzad03x@gmail.com>
2026-07-07 19:47:45 +0330

    Delete payload.js

--- payload.js
+++
@@ -1,6 +0,0 @@
-function load_payload() {
-    document.body.innerHTML += "<p>Loading GoldHEN...</p>";
-    setTimeout(function(){
-        document.body.innerHTML += "<p>Done ✔</p>";
-    }, 2000);
-}
