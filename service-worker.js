37d0c0824405ab066a67b2f6971544fd94ecaf36
gameland125.github.io <farzad03x@gmail.com>
2026-07-17 09:13:39 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -61,55 +61,68 @@ const APP_SHELL = [
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(APP_SHELL);
-    }).then(function () {
-      return self.skipWaiting();
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(APP_SHELL);
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
   );
 });
 
 self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(
-        keyList.map(function (key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(function () {
-      return self.clients.claim();
-    })
+    caches
+      .keys()
+      .then(function (keyList) {
+        return Promise.all(
+          keyList.map(function (key) {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
   );
 });
 
 self.addEventListener('fetch', function (event) {
-  if (event.request.method !== 'GET') return;
+  if (event.request.method !== 'GET') {
+    return;
+  }
 
   event.respondWith(
     caches.match(event.request).then(function (cached) {
-      if (cached) return cached;
+      if (cached) {
+        return cached;
+      }
 
-      return fetch(event.request).then(function (response) {
-        if (
-          response &&
-          response.status === 200 &&
-          response.type === 'basic'
-        ) {
-          const responseClone = response.clone();
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseClone);
+      return fetch(event.request)
+        .then(function (response) {
+          if (response && response.status === 200 && response.type === 'basic') {
+            var responseClone = response.clone();
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(event.request, responseClone);
+            });
+          }
+
+          return response;
+        })
+        .catch(function () {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+
+          return new Response('', {
+            status: 504,
+            statusText: 'Offline'
           });
-        }
-        return response;
-      }).catch(function () {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-        return new Response('', { status: 504, statusText: 'Offline' });
-      });
+        });
     })
   );
 });
