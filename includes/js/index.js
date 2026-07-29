59a99efe0dc3f04c5fe19e544d3952e23c1ec2f6
gameland125.github.io <farzad03x@gmail.com>
2026-07-29 23:43:52 +0330

    Update index.js

--- includes/js/index.js
+++ includes/js/index.js
@@ -441,23 +441,31 @@ function DisplayCacheProgress() {
   }, 2000);
 }
 
-function terminateCache() {
-  if (window.applicationCache) {
-    // Status 3 is 'downloading', Status 1 is 'checking'
-    if (window.applicationCache.status === 3 || window.applicationCache.status === 1) {
-      console.log("Terminating cache process to save memory...");
-      window.applicationCache.abort();
+function showExitScreen() {
+  document.body.style.background = '#000';
+  document.body.style.margin = '0';
+  document.body.innerHTML = `
+    <div style="color:#fff;text-align:center;font-size:26px;margin-top:50px">
+      GoldHEN Loaded<br>در حال خروج...
+    </div>
+  `;
 
-      // restore title
-      document.title = window.lang.title || "PSFree Enhanced";
-
-      // cleanup
-      window.applicationCache.removeEventListener("progress", DLProgress);
-      window.applicationCache.oncached = null;
-      window.applicationCache.onupdateready = null;
+  // اگر در حال نصب/دانلود کش آفلاین هستیم، کلوز نکن
+  try {
+    if (window.applicationCache &&
+        (applicationCache.status === applicationCache.CHECKING ||
+         applicationCache.status === applicationCache.DOWNLOADING)) {
+      return;
     }
-  }
+  } catch (e) {}
+
+  // فقط بعد از موفقیت جیلبریک کلوز کن
+  setTimeout(() => {
+    window.open('', '_self', '');
+    window.close();
+  }, 2000);
 }
+
 
 
 function setAdvancedPayloads(inputState) {
