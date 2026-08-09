cf675e3e3352168e4073d6b322062f945a966441
gameland125.github.io <farzad03x@gmail.com>
2026-08-09 08:04:30 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,5 @@
-const CACHE_NAME = 'gameland-cache-v1';
-
-const ASSETS = [
-    './',
-    './index.html',
-    './bundle.js'
-];
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(ASSETS);
-        })
-    );
-
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function (keys) {
-                return Promise.all(
-                    keys
-                        .filter(function (key) {
-                            return key !== CACHE_NAME;
-                        })
-                        .map(function (key) {
-                            return caches.delete(key);
-                        })
-                );
-            })
-            .then(function () {
-                return self.clients.claim();
-            })
-    );
-});
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (cachedResponse) {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request)
-                    .catch(function () {
-                        return caches.match('./index.html');
-                    });
-            })
-    );
-});
+const CACHE_NAME = 'gameland-safe-clean-v2';
+const ASSETS = ['./','./index.html','./bundle.js','./sw.js','./payloads/payloads.js'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { if (event.request.method === 'GET' && response.ok) { const copy=response.clone(); caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)); } return response; }).catch(() => caches.match('./index.html')))); });
